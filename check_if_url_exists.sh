798bf1de8b2509e7c5dceeb8dc0e64d416137c5f
ckrmml <ckrummel@mail.de>
2021-10-15 21:24:31 +0200

    Add files via upload

--- check_if_url_exists.sh
+++ check_if_url_exists.sh
@@ -1,11 +1,10 @@
-#!/bin/sh 
+#!/usr/bin/env bash
 
-for URL in $(cat newsurls.txt) ; do 
-    res="$(curl --silent --head --location --output /dev/null --write-out '%{http_code}' $URL)"
+while read -r URL; do 
+    res="$(curl --silent --head --location --output /dev/null --write-out '%{http_code}' "$URL")"
     if [[ "$res" == 200 ]]; then 
-        curl -s -L -I -o /dev/null -w $URL' -> %{url_effective}\n' $URL; 
+        curl -s -L -I -o /dev/null -w "$URL"' -> %{url_effective}\n' "$URL"; 
     else
         echo "$URL -> "No exist"" 
     fi 
-done
-
+done < "$1" 
